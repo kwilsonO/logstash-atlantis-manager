3c0c55ce8b693be2ed0c7a865189bea04fb1295f
root <root@ip-10-144-206-29.ec2.internal>
2015-09-03 21:19:06 -0400

    fixed bugs and added script to delte old data

--- scripts/api/clean-up.sh
+++ scripts/api/clean-up.sh
@@ -4,10 +4,12 @@ DATADIR="${REPOPATH}/data"
 SUPDIR="${DATADIR}/supervisors"
 CONDIR="${DATADIR}/containers"
 
-for d in $CONDIR/; do
+for d in $CONDIR/*; do
 	if [ -d $d ]; then
-		(ls -t $d | head -n 5; ls)|sort|uniq -u|xargs --no-run-if-empty rm
+		cd $d
+		(ls -t | head -n 5; ls)|sort|uniq -u|xargs --no-run-if-empty rm 
 	fi
 done
 
-(ls -t $SUPDIR | head -n 5; ls)|sort|uniq -u|xargs --no-run-if-empty rm
+cd $SUPDIR
+(ls -t | head -n 5; ls)|sort|uniq -u|xargs --no-run-if-empty rm
